79f3748d254e6b0a5c7e8ea547a4e3afbf19d9da
Sulamita Santos <95324858+SulamitaSan@users.noreply.github.com>
2022-11-06 21:41:32 -0300

    Add files via upload

---
+++ AgenciaBackEnd/AgenciaBackEnd/Models/DestinoDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace AgenciaBackEnd.Models
+{
+    public class DestinoDBContext : DbContext
+    {
+        public DestinoDBContext(DbContextOptions<DestinoDBContext> options)
+            : base(options)
+            { }
+        public DbSet<Destino> Destino { get; set; }
+
+    }
+}
